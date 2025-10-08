365390e83917dd415105aa8d5bcaff28594711e4
Lauren Lanning <lauren.lanning@student.fairfield.edu>
2025-10-08 09:46:33 -0400

    Update Main.java
    This takes files and creates a clean list, now we just need to remove the stop words!

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        FileLoader loader = new FileLoader();
-        ArrayList<String> words = loader.loadWords();
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        FileLoader file1 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle1.txt");
+        FileLoader file2 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle2.txt");
+        FileLoader file3 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle3.txt");
+
+        file1.loadFile();
+        file2.loadFile();
+        file3.loadFile();
+
+        //StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt");
 
 
-        StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt");
+       // ArrayList<String> filtered = new ArrayList<>();
+       // for (String w : words) {
+       //     if (!stop.isStopWord(w)) {
+       //         filtered.add(w);
+       //     }
+       // }
 
+        //ArrayList<String> unique = new ArrayList<>();
+        //for (String w : filtered) {
+        //    if (!unique.contains(w)) {
+        //        unique.add(w);
+        //    }
+        //}
 
-        ArrayList<String> filtered = new ArrayList<>();
-        for (String w : words) {
-            if (!stop.isStopWord(w)) {
-                filtered.add(w);
-            }
-        }
-
-        ArrayList<String> unique = new ArrayList<>();
-        for (String w : filtered) {
-            if (!unique.contains(w)) {
-                unique.add(w);
-            }
-        }
-
-        System.out.println("Total words: " + words.size());
-        System.out.println("  ");
-        System.out.println("After removing stopwords: " + filtered.size());
-        System.out.println("  ");
-        System.out.println("Unique words: " + unique.size());
+        //System.out.println("Total words: " + words.size());
+        //System.out.println("");
+        //System.out.println("After removing stopwords: " + filtered.size());
+        //System.out.println("");
+        //System.out.println("Unique words: " + unique.size());
     }
 }
-
-
