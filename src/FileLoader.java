988af69c1e4eea7fea6dcef9952f0ae30a98c186
Lauren Lanning <lauren.lanning@student.fairfield.edu>
2025-10-08 09:38:42 -0400

    Update FileLoader.java

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -7,23 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileLoader {
+private String filename;
 
-    public static void main(String[] args) throws Exception {
-        //first dog article
-        String filePath1 = "C:/Users//laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle1.txt";
-        String article1 = "";
-        try {
-            // Read all lines from the text file into a List
-            List<String> lines1 = Files.readAllLines(Paths.get(filePath1));
-            for (String line : lines1) {
-                article1 += line + " ";
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading the file: " + e.getMessage());
+public FileLoader(String filename) {
+    this.filename = filename;
+}
+
+private void loadFile() {
+    String filePath1 = filename;
+    String article1 = "";
+    try {
+        // Read all lines from the text file into a List
+        List<String> lines1 = Files.readAllLines(Paths.get(filePath1));
+        for (String line : lines1) {
+            article1 += line + " ";
         }
-        article1 = article1.replaceAll("[^a-zA-Z0-9\\s]", "");
-        String[] a1 = article1.split("\\s+");
-        ArrayList<String> dogArticle1 = new ArrayList<>(Arrays.asList(a1));
-        System.out.println("Article 1: " + dogArticle1);
+    } catch (IOException e) {
+        System.out.println("Error reading the file: " + e.getMessage());
     }
+    article1 = article1.replaceAll("[^a-zA-Z0-9\\s]", "");
+    String[] a1 = article1.split("\\s+");
+    ArrayList<String> dogArticle1 = new ArrayList<>(Arrays.asList(a1));
+    System.out.println("Article 1: " + dogArticle1);
+}
 }
