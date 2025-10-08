9fce223c5241cd763c6df9aa28fd0cce9a518777
ijake1 <jake.samuel@student.fairfield.edu>
2025-10-08 08:56:28 -0400

    fixed so now it takes away the stopwords when u run it

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,22 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception {
         FileLoader loader = new FileLoader();
-
         ArrayList<String> words = loader.loadWords();
 
-        System.out.println("\n\nLoaded " + words.size() + " words total from the articles.");
+
+        StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt");
+
+
+        ArrayList<String> filtered = new ArrayList<>();
+        for (String w : words) {
+            if (!stop.isStopWord(w)) {
+                filtered.add(w);
+            }
+        }
+
+
+        System.out.println("Total words: " + words.size());
+        System.out.println("After removing stopwords: " + filtered.size());
     }
 }
 
