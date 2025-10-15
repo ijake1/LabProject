13109ba3137e573e6d270225a1a2d90888bf280b
Lauren Lanning <lauren.lanning@student.fairfield.edu>
2025-10-15 09:20:28 -0400

    Update FileLoader.java

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -6,36 +6,51 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 public class FileLoader {
     private ArrayList<String> words = new ArrayList<>();
     private String filename;
 
-public FileLoader(String filename) {
-    this.filename = filename;
-}
-
-public void loadFile() {
-    String filePath = filename;
-    String article = "";
-    try {
-        List<String> lines = Files.readAllLines(Paths.get(filePath));
-        for (String line : lines) {
-            article += line + " ";
-        }
-    } catch (IOException e) {
-        System.out.println("Error reading the file: " + e.getMessage());
+    public FileLoader(String filename) {
+        this.filename = filename;
     }
-    article = article.replaceAll("[^a-zA-Z0-9\\s]", "");
-    String[] a = article.split("\\s+");
-    ArrayList<String> articleList = new ArrayList<>(Arrays.asList(a));
-    words.addAll(articleList);
-    System.out.println(articleList);
-}
+
+    public void loadFile() {
+        String filePath = filename;
+        String article = "";
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for (String line : lines) {
+                article += line + " ";
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+        article = article.replaceAll("[^a-zA-Z0-9\\s]", "");
+        String[] a = article.split("\\s+");
+        ArrayList<String> articleList = new ArrayList<>(Arrays.asList(a));
+        words.addAll(articleList);
+        System.out.println(articleList);
+    }
     public ArrayList<String> getWords() {
         return words;
     }
 
-}
+    public ArrayList<String> removeStopWords(StopWords stop) {
+        ArrayList<String> filtered = new ArrayList<>();
+        for (String w : words) {
+            if (!stop.isStopWord(w)) {
+                filtered.add(w);
+            }
+        }
+        return filtered;
+    }
 
-
+    public ArrayList<String> uniqueWords(ArrayList<String> filteredList) {
+        ArrayList<String> unique = new ArrayList<>();
+        for (String w : filteredList) {
+            if (!unique.contains(w)) {
+                unique.add(w);
+            }
+        }
+        return unique;
+    }
