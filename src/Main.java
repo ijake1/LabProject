66a4d4e530b8906b21966bf8089ba731ef123c35
Lauren Lanning <lauren.lanning@student.fairfield.edu>
2025-10-15 09:19:38 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,40 @@
-import java.io.*;
 import java.util.*;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        //FileLoader file1 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle1.txt");
-        //FileLoader file2 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle2.txt");
-        //FileLoader file3 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle3.txt");
+        FileLoader file1 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle1.txt");
+        FileLoader file2 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle2.txt");
+        FileLoader file3 = new FileLoader("C:/Users/laure/OneDrive/Documents/Programming Lab/Dog Articles/DogArticle3.txt");
 
-        FileLoader file1 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle1.txt");
-        FileLoader file2 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle2.txt");
-        FileLoader file3 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle3.txt");
-
+        //FileLoader file1 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle1.txt");
+        //FileLoader file2 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle2.txt");
+        //FileLoader file3 = new FileLoader("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/Articles/Catarticle3.txt");
 
         file1.loadFile();
         file2.loadFile();
         file3.loadFile();
 
-        ArrayList<String> words = new ArrayList<>();
-        words.addAll(file1.getWords());
-        words.addAll(file2.getWords());
-        words.addAll(file3.getWords());
+        System.out.println();
+        System.out.println("File 1 Total Words: " + file1.getWords().size());
+        System.out.println("File 2 Total Words: " + file2.getWords().size());
+        System.out.println("File 3 Total Words: " + file3.getWords().size());
 
+        //StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt");
+        StopWords stop = new StopWords("C:/Users/laure/OneDrive/Documents/Programming Lab/stopwords.txt");
 
-        StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt");
-        //StopWords stop = new StopWords("C:/Users/jakes/OneDrive/Desktop/CPSC 2231/Lab Project/stopwords.txt"); - put your stopwords.txt file path here
+        ArrayList<String> file1R = new ArrayList<>(file1.removeStopWords(stop));
+        ArrayList<String> file2R = new ArrayList<>(file2.removeStopWords(stop));
+        ArrayList<String> file3R = new ArrayList<>(file2.removeStopWords(stop));
 
-       ArrayList<String> filtered = new ArrayList<>();
-       for (String w : words) {
-            if (!stop.isStopWord(w)) {
-                filtered.add(w);
-            }
-        }
+        System.out.println();
+        System.out.println("File 1 Total Words After Removing Stop Words: " + file1R.size());
+        System.out.println("File 2 Total Words After Removing Stop Words: " + file2R.size());
+        System.out.println("File 3 Total Words After Removing Stop Words: " + file3R.size());
 
-        ArrayList<String> unique = new ArrayList<>();
-        for (String w : filtered) {
-            if (!unique.contains(w)) {
-                unique.add(w);
-            }
-        }
-
-        System.out.println("");
-        System.out.println("Total words: " + words.size());
-        System.out.println("");
-        System.out.println("After removing stopwords: " + filtered.size());
-        System.out.println("");
-        System.out.println("Unique words: " + unique.size());
+        System.out.println("File 1 Total Unique Words: " + uniqueWords(file1R));
+        System.out.println("File 2 Total Unique Words: " + uniqueWords(file2R));
+        System.out.println("File 3 Total Unique Words: " + uniqueWords(file3R));
     }
 }
