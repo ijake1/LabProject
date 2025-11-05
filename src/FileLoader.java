554c194a29b5cf0f0df08ae6d62add83d8613930
Lauren Lanning <lauren.lanning@student.fairfield.edu>
2025-11-05 08:15:30 -0500

    Update FileLoader.java

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -6,14 +6,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/**
+ * The FileLoader class reads the contents of a text file,
+ * cleans the text by removing punctuation, and stores all words
+ * in an ArrayList<String> for later use.
+ */
 public class FileLoader {
+    // Stores all words extracted from the file
     private ArrayList<String> words = new ArrayList<>();
+    // The name (or path) of the file to be read
     private String filename;
 
+    // Constructor that accepts a file name or path
     public FileLoader(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * Reads the file line by line, concatenates its contents,
+     * removes punctuation, splits the text into words,
+     * and stores them in an ArrayList.
+     *
+     * @return an ArrayList<String> containing all cleaned words from the file
+     */
     public ArrayList<String> loadFile() {
         String filePath = filename;
         String article = "";
